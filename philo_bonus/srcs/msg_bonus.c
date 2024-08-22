010a19a4ffd7ff02c8c24517f52dba8cc11db4d9
Kha Tieu <tieuduykha1810@gmail.com>
2024-08-22 18:36:33 +0300

    finished

--- philo_bonus/srcs/msg_bonus.c
+++ philo_bonus/srcs/msg_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 13:17:18 by ktieu             #+#    #+#             */
-/*   Updated: 2024/08/22 17:17:44 by ktieu            ###   ########.fr       */
+/*   Updated: 2024/08/22 17:58:56 by ktieu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,15 @@ int	philo_msg(t_philo *philo, char *str)
 {
 	size_t	time;
 
-	time = get_current_time() - philo->start_ms;
 	if (ft_check_terminate(philo))
 		return (1);
+	
 	if (sem_wait(philo->prog->sem_print) != 0)
 	{
 		error_msg("philo_msg: sem_wait()\n");
 		exit(1);
 	}
+	time = get_current_time() - philo->start_ms;
 	printf("%zu %d %s\n", time, philo->id + 1, str);
 	if (sem_post(philo->prog->sem_print) != 0)
 	{
